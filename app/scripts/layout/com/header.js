6234e42d7e917eb7ef7acc8341b53f2a6cd525ca
Eric <eric@combojs.com>
2018-01-19 15:06:53 -0600

    Fixed unfluid image

--- app/scripts/layout/com/header.js
+++ app/scripts/layout/com/header.js
@@ -3,7 +3,7 @@ const Header = new class extends Combo.Component {
 		return `
 			<header class="bloom-section custom-section--header custom-shadow--4">
 				<div class="bloom-section__body">
-					<img src="images/logo.png">
+					<img src="images/logo.png" style="max-width: 100%; height:auto">
 					<p>
 						<a class="bloom-button custom-button--header" href="https://github.com/combojs/combo-js/blob/master/doc/api.md">API</a>
 						<a class="bloom-button custom-button--header" href="https://github.com/combojs/combo-seed">Boilerplate</a>
